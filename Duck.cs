292e07ff6743aa5303654af78fd5e1e746dd6301
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-06 20:50:39 -0500

    Add project files.

---
+++ Duck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfDucks
+{
+    internal class Duck : IComparable<Duck>
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (this.Size>duckToCompare.Size)
+            {
+                return 1;
+            }
+            else if (this.Size < duckToCompare.Size)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ListOfDucks
+{
+    internal class Program
+    {
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine($"{duck.Size} inch {duck.Kind}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){Kind=KindOfDuck.Mallard,Size=17},
+                new Duck(){ Kind=KindOfDuck.Muscovy,Size=18},
+                new Duck(){ Kind=KindOfDuck.Loon,Size=14},
+                new Duck(){ Kind=KindOfDuck.Muscovy,Size=11},
+                new Duck(){ Kind=KindOfDuck.Mallard,Size=14},
+                new Duck(){Kind=KindOfDuck.Loon,Size=13},
+            };
+            ducks.Sort();
+            PrintDucks(ducks);
+        }
+    }
+}
